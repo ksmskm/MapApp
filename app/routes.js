a48488c84c599a3a9ac2334824141add2feb01cf
Kyle Marvich <ksmarvich@gmail.com>
2016-03-18 00:08:50 -0700

    initial map setup

---
+++ app/routes.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var User = require('./model.js');
+
+function routes(app) {
+  app.get('/users', function(req, res) {
+    var query = User.find({});
+    query.exec(function(err, users) {
+      if (err) res.send(err);
+      res.json(users);
+    });
+  });
+
+  app.post('/users', function(req, res) {
+    var newuser = new User(req.body);
+
+    newuser.save(function(err) {
+      if (err) res.send(err);
+      res.json(req.body);      
+    });
+  });
+}
+
+module.exports = routes;
---
+++ public/js/addCtrl.js
@@ -0,0 +1,34 @@
+var addCtrl = angular.module('addCtrl', ['geolocation', 'gservice']);
+
+addCtrl.controller('addCtrl', function($scope, $http, geolocation, gservice) {
+  $scope.formData = {};
+  var coords = {};
+  var lat = 0;
+  var long = 0;
+
+  $scope.formData.latitude = 39.500;
+  $scope.formData.longitude = -98.350;
+
+  $scope.createUser = function() {
+    var userData = {
+      username: $scope.formData.username,
+      gender: $scope.formData.gender,
+      age: $scope.formData.age,
+      favlang: $scope.formData.favlang,
+      location: [$scope.formData.longitude, $scope.formData.latitude],
+      htmlverified: $scope.formData.htmlverified
+    };
+
+    $http.post('/users', userData)
+      .success(function (data) {
+        $scope.formData.username = "";
+        $scope.formData.gender = "";
+        $scope.formData.age = "";
+        $scope.formData.favlang = "";
+        gservice.refresh($scope.formData.latitude, formData.longitude);
+      })
+      .error(function (data) {
+        console.log('Error: ' + data);
+      });
+  };
+});
